637b0b838879e40983e473409b2fbe0c6b75250d
maugern <n-mauger@hotmail.fr>
2015-03-26 23:11:22 +0100

    Uplate main.java pour l'exemple de javadoc
    wuesh sa fait quoi si j'écris ici?

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
+/**
+ * @author
+ */
+
 public class Main {
+	/**
+	 *C'est la valeur exacte de PI
+	*/
+	private int PI = 56.8;
+	
+	/**
+         * La méthode cacahuete sert a vérifier si la cacahuete choisi est la réponse à la vie.
+         * 
+         * @param Nom
+         * 		Le nouveau nom de la cacahuete.
+         * 
+         * @return La réponse à la vie
+         * 		
+         */
+	public int cacahuete(String lol){
+		
+	}
+	
+	/**
+	 * C'est le main
+	 */
 	public static void main(String[] args) {
 
 	}
