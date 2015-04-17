7516941ea310bfeb7c5b9db8414e8f57a50e861d
maugern <n-mauger@hotmail.fr>
2015-04-17 15:22:29 +0200

    Ajout de javadoc

--- src/Position.java
+++ src/Position.java
@@ -1,3 +1,7 @@
+/**
+ * @author Nicolas Mauger lol 
+ */
+
 public class Position {
 
 	private int x;
@@ -8,6 +12,13 @@ public class Position {
 	private boolean obstacle = false;
 	private static Plateau p;
 
+	/**
+	 * Constructeur de la classe Plateau
+	 * @param x
+	 * 		Coordonnée de la position en X
+	 * @param y
+	 * 		Coordonnée de la position en y
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -33,6 +44,11 @@ public class Position {
 		return this.obstacle;
 	}
 
+	/**
+	 * Fonction qui change en base la position si elle n'est pas base, et inversement
+	 * @param equipe
+	 * 		L'équipe à qui appartient la base 		
+	 */
 	public void flipBase(int equipe) {
 		if (!this.base) {
 			this.base = !this.base;
@@ -42,7 +58,12 @@ public class Position {
 			this.setEquipe(2);
 		}
 	}
-
+	
+	/**
+	 * Fonction qui change en mine la position si elle n'est pas mine, et inversement
+	 * @param equipe
+	 * 		L'équipe à qui appartient la mine
+	 */
 	public void flipMine(int equipe) {
 		if (!this.mine) {
 			this.mine = !this.mine;
@@ -52,11 +73,19 @@ public class Position {
 			this.setEquipe(2);
 		}
 	}
-
+	
+	/**
+	 * Procedure qui change la position en obstacle si elle ne l'est pas, et inversement
+	 */ 
 	public void flipObstacle() {
 		this.obstacle = !this.obstacle;
 	}
-
+	
+	/**
+	 * Fonction qui indique si la position est robot ou non
+	 * @return boolean
+	 * 		Retourne true si la position contient un robot
+	 */
 	public boolean estRobot() {
 		for (int i = 0; i < p.getListeRobot().size(); i++) {
 			if (p.getListeRobot().get(i).getPosition().getX() == this.getX()
@@ -68,7 +97,6 @@ public class Position {
 	}
 
 	public Robot getRobot() {
-
 		for (int i = 0; i < p.getListeRobot().size(); i++) {
 			if (p.getListeRobot().get(i).getPosition().equals(this))
 				return p.getListeRobot().get(i);
@@ -81,6 +109,11 @@ public class Position {
 		p = pl;
 	}
 
+	/**
+	 * Fonction qui retourne le contenu de la position en ascii art
+	 * @return String
+	 * 		B si c'est une base, M si c'est une mine, # si c'est un obstacle
+	 */
 	public String getContenu() {
 		if (this.estBase()) {
 			return "B ";
