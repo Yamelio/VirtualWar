04482d4a211150cc9332f6460e4a27f9b8fd0620
Nicolas Mauger <n-mauger@hotmail.fr>
2015-05-27 23:14:08 +0200

    ajout accueil

--- src/Parametrage.java
+++ src/Parametrage.java
@@ -126,14 +126,6 @@ public class Parametrage {
 		f.pack();
 	}
 
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new Parametrage();
-			}
-		});
-	}
-
 	public int getHauteur() {
 		return hauteurPlateau;
 	}
