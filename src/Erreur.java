1fe9112457b907e8cf47a75e6599c9fcc7736722
Nicolas Mauger <n-mauger@hotmail.fr>
2015-06-04 19:31:45 +0200

    Android 2

--- src/Erreur.java
+++ src/Erreur.java
@@ -12,7 +12,7 @@ public class Erreur extends Exception {
 	
 
 	public void getG01(){
-		JOptionPane.showMessageDialog(null, "Erreur G01 : Sélection du robot impossible", "VirtualWar", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Erreur G01 : SÃ©lection du robot impossible", "VirtualWar", JOptionPane.ERROR_MESSAGE);
 	}
 	
 	public void getG02(){
@@ -28,13 +28,15 @@ public class Erreur extends Exception {
 	}
 	
 	public void getG05(){
-		JOptionPane.showMessageDialog(null, "Erreur G05 : Impossible de se déplacer dans cette direction", "VirtualWar", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Erreur G05 : Impossible de se dÃ©placer dans cette direction", "VirtualWar", JOptionPane.ERROR_MESSAGE);
 	}
 	
 	public void getT01(){
-		JOptionPane.showMessageDialog(null, "Erreur T01 : Caractère incompatible!\n Veuillez enter un nombre entier.", "VirtualWar", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Erreur T01 : CaractÃ¨re incompatible!\n Veuillez entrer un nombre entier.", "VirtualWar", JOptionPane.ERROR_MESSAGE);
 	}
 
 	public void getT02(){
+		JOptionPane.showMessageDialog(null, "Erreur T02 : Valeur incompatible!\n Veuillez entrer dans l'intervalle correct.", "VirtualWar", JOptionPane.ERROR_MESSAGE);
+	}
 
 }
